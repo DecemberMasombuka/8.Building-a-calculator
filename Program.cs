368aee92c6926f32cfdf9e9c635e450ad29d5c02
A252422 <A252422@LSBSA-19Y5HW3>
2024-01-10 12:38:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Building_a_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two integers to add");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+            
+            Console.WriteLine("Enter Two decimasl to add");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            double num4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num3 + num4);
+            Console.Read();
+        }
+    }
+}
